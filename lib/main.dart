fd01a9a9d697dc200edffd7bf5064b59a5fb3ebd
IMassaror <igormassaro@hotmail.com>
2022-06-01 10:47:27 -0300

    Em algum pé

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:cloud_functions/cloud_functions.dart';
+//import 'package:firebase_core/firebase_core.dart';
+//import 'package:firebase_storage/firebase_storage.dart';
+//import 'package:cloud_functions/cloud_functions.dart';
 import 'firebase_options.dart';
 import 'credit_card_page.dart';
 
